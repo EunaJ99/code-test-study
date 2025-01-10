3fc188d2fdc1c17527f5dd8f45f9783f750180ae
eunaj99 <esfjge@gmail.com>
2025-01-10 18:14:59 +0900

    refactor: 백준 11724 연결 요소의 개수
    실행 시간을 1664ms에서 1432ms로 줄임.

--- src/main/java/org/baekjoon/BKJ11724.java
+++ src/main/java/org/baekjoon/BKJ11724.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class BKJ11724 {
     static boolean[] visited;
-    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static int[][] graph;
 
     public static void main(String[] args) {
         BKJ11724 m = new BKJ11724();
@@ -12,31 +12,31 @@ public class BKJ11724 {
         int nodeCount = sc.nextInt();
         int edgeCount = sc.nextInt();
         int answer = 0;
-        for (int i = 0; i <= nodeCount; i++) {
-            graph.add(new ArrayList<>());
-        }
+        graph = new int[nodeCount+1][nodeCount+1];
         visited = new boolean[nodeCount+1];
         for (int i = 0; i < edgeCount; i++) {
             int firstNode = sc.nextInt();
             int secondNode = sc.nextInt();
-            graph.get(firstNode).add(secondNode);
-            graph.get(secondNode).add(firstNode);
+            graph[firstNode][secondNode] = 1;
+            graph[secondNode][firstNode] = 1;
         }
 
         for (int n = 1; n <= nodeCount; n++) {
-            if (visited[n]) { continue; }
-            m.DFS(n);
-            answer++;
+            if (!visited[n]) {
+                m.DFS(n);
+                answer++; }
         }
 
         System.out.println(answer);
     }
 
     void DFS(int n) {
+        if (visited[n]) { return; }
         visited[n] = true;
-        for (int j: graph.get(n)) {
-            if (visited[j]) { continue; }
-            DFS(j);
+        for (int i = 0; i < visited.length; i++) {
+            if (graph[n][i] == 1 && !visited[i]) {
+                DFS(i);
+            }
         }
     }
 }
